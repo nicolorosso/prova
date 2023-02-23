6956fa29b4827fcc7ca75d6e98b1a7278ec37fb3
Nicolò Rosso <98038058+nicolorosso@users.noreply.github.com>
2023-02-23 12:50:14 +0100

    Update prova.py

--- prova.py
+++ prova.py
@@ -1,4 +1,4 @@
-import openpyxl
+#import openpyxl
 import pandas as pd
 import snscrape.modules.twitter as sntwitter
 #import spacy
@@ -7,11 +7,7 @@ import datetime
 import re
 import xlsxwriter
 from io import BytesIO
-import requests
 
-
-url = 'https://github.com/nicolorosso/prova/blob/main/Parlamento_sito_ADL.xlsx?raw=true'
-myfile = requests.get(url)
 
 import concurrent.futures
 
@@ -49,8 +45,8 @@ col1.header('Opzioni Ricerca')
 
 
 ## Sidebar
-account = pd.read_excel(myfile.content)
-#governo= pd.read_excel(r'C:\Users\nickr\Downloads\account_governo_twitter.xlsx', sheet_name = 'Sheet1', engine='openpyxl')
+account = pd.read_excel(r"C:\Users\nickr\OneDrive\Desktop\ADL\Parlamento_sito_ADL.xlsx", sheet_name= 'STAKEHOLDER')
+governo= pd.read_excel(r'C:\Users\nickr\Downloads\account_governo_twitter.xlsx', sheet_name = 'Sheet1')
 
 account['username'] = account['LINK PAGINA TWITTER'].str.split('/').str[-1]
 account['username'] = account['username'].str.split('?').str[0]
@@ -58,8 +54,8 @@ account = account.dropna(subset=['username'])
 account['nome_cognome'] = account['NOME'].str.cat(account['COGNOME'], sep=' ')
 account = account.reset_index()
 
-#governo['LINK PAGINA TWITTER'] = governo['LINK PAGINA TWITTER'].str.split('/').str[-1]
-#governo['LINK PAGINA TWITTER'] = governo['LINK PAGINA TWITTER'].str.split('?').str[0]
+governo['LINK PAGINA TWITTER'] = governo['LINK PAGINA TWITTER'].str.split('/').str[-1]
+governo['LINK PAGINA TWITTER'] = governo['LINK PAGINA TWITTER'].str.split('?').str[0]
 
 
 start_date = st.sidebar.date_input("Start date", datetime.date(2013, 1, 1))
@@ -72,8 +68,6 @@ end_date = str(end_date)
 options = account['nome_cognome'].tolist()
 
 # Display the dropdown menu and store the selected values in a list called 'selected_options'
-#selected_options_indexes = st.sidebar.multiselect('Scegli il politico', options)
-#selected_options = account.loc[account['nome_cognome'].isin(selected_options_indexes), 'username'].tolist()
 container = st.sidebar.container()
 #all = st.sidebar.checkbox("Select all")
 
@@ -95,24 +89,13 @@ else:
     selected_usernames = account.loc[selected_options, 'username'].tolist()
 
 
-# Get the corresponding usernames for the selected options
-#selected_usernames = account.loc[selected_options, 'username'].tolist()
-topics = st.sidebar.text_input('Scrivere le parole chiave separate da una virgola:', "")
 
+topics = list((num) for num in st.sidebar.text_input('Scrivere le parole chiave separate da una virgola:', "").strip().split(","))
 
-numero = st.sidebar.slider('Numero di Tweet', 10, 100, 100)
-
-#---------------------------------------------------------------------------------------------#
-#ricerca_governo = st.sidebar.text('Spunta la casella per includere nella ricerca tutti i membri del governo')
-
-
-#with st.sidebar:
-    #with st.form(key='governo'):
-       # governo_button = st.form_submit_button(label='Seleziona per ricerca governo')
+numero = st.sidebar.slider('Numero di Tweet', 10, 100, 25)
 
 
 with st.sidebar:
-
     with st.form(key='my_form'):
         submit_button = st.form_submit_button(label='Invia')
 
@@ -123,45 +106,48 @@ def scrape_tweets_for_user(topic, username, since, until):
     for i, tweet in enumerate(sntwitter.TwitterSearchScraper(f"{topic} from:{username} since:{since} until:{until}").get_items()):
         if i>100:
             break
-        tweets_list1.append([tweet.url,
+        tweets_list1.append([tweet.user.displayname,
                             tweet.date,
-                            tweet.id,
-                            tweet.content,
-                            tweet.user.username
+                            tweet.rawContent,
+                             tweet.id,
+                            tweet.url
                               ])
     return tweets_list1
 
 def scraper(parole, users, since, until):
     tweets_list1 = []
     with concurrent.futures.ProcessPoolExecutor(30) as executor:
-        results = [executor.submit(scrape_tweets_for_user, parole, n, since, until) for p in parole for n in users]
+        results = [executor.submit(scrape_tweets_for_user, p, n, since, until) for p in parole for n in users]
         for future in concurrent.futures.as_completed(results):
             tweets_list1.extend(future.result())
 
-    df = pd.DataFrame(tweets_list1, columns=['Url',
-                                              'Datetime',
-                                              'Tweet Id',
+    df = pd.DataFrame(tweets_list1, columns=['Nome Utente',
+                                              'Data',
                                               'Text',
-                                              'nome utente'
+                                             'Tweet ID',
+                                              'URL'
                                               ])
-    df['Datetime'] = df['Datetime'].dt.date
-    df.sort_values(by='Datetime', ascending=False, inplace=False)
-    st.dataframe(df)
+    if df.empty:
+        st.error("Non sono stati trovati Tweet con le parole chiave selezionate!", icon="🚨")
+    else:
+        df['Data'] = df['Data'].dt.date
+        df.sort_values(by='Data', ascending=False, inplace=False)
+        st.dataframe(df)
 
-    output = BytesIO()
-    # Write files to in-memory strings using BytesIO
+        # Write files to in-memory strings using BytesIO
+        output = BytesIO()
 
-    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
-        # Write each dataframe to a different worksheet.
-        df.to_excel(writer, sheet_name='Dati Scraper', index=False)
 
-        writer.save()
+        with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+            # Write each dataframe to a different worksheet.
+            df.to_excel(writer, sheet_name='Dati Scraper', index=False)
+            writer.save()
 
-    st.download_button(
-        label="Download foglio Excel",
-        data=output.getvalue(),
-        file_name="scraper.xlsx",
-        mime="application/vnd.ms-excel")
+            st.download_button(
+                label="Download foglio Excel",
+                data=output.getvalue(),
+                file_name="scraper.xlsx",
+                mime="application/vnd.ms-excel")
 
 
 list_of_users = account['username'].tolist()
@@ -171,7 +157,7 @@ until = "2022-12-29"
 
 list_of_parole = ['Fondazione De Gasperi', 'Lorenzo Malagola']
 if submit_button:
-    scraper(topics.split(','), selected_usernames, start_date, end_date)
+    scraper(topics, selected_usernames, start_date, end_date)
 
 
 #if governo_button:
